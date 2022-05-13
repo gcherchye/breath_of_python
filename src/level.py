86a3935adb4f301448ebd5a4a3fac000b9b95cb4
GoodyeAAr <gauthier.cherchye@partenamut.be>
2022-05-13 15:09:31 +0200

    feat(map): add floor map

--- src/level.py
+++ src/level.py
@@ -58,11 +58,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        # Creating the floor
+        self.floor_surf = pygame.image.load('lib/images/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect()
+
     def custom_draw(self, player):
         """docstring"""
         # Getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # Drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
