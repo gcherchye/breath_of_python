7de25e636e472372a1a4a7a3680e8da929e9bdf3
GoodyeAAr <gauthier.cherchye@partenamut.be>
2022-05-13 13:37:06 +0200

    ref(player): fix import bug

--- src/player.py
+++ src/player.py
@@ -1,14 +1,16 @@
 """docstring goes here"""
 from __future__ import absolute_import
 
+from typing import List, Tuple
+
 import pygame
 
 
-class Tile(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     """docstring goes here"""
 
-    def __init__(self, pos: int, groups: pygame.sprite.Group) -> None:
+    def __init__(self, pos: Tuple[int, int], groups: List[pygame.sprite.Group]) -> None:
         super().__init__(groups)
 
-        self.image = pygame.image.load('../lib/images/dummy/player.png').convert_alpha()
+        self.image = pygame.image.load('lib/images/dummy/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
