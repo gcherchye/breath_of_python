6d519f1dded39f226640062035d7bdba00c747fe
GoodyeAAr <gauthier.cherchye@partenamut.be>
2022-05-25 12:35:45 +0200

    bug(tile): fix placement issue for objects

--- src/tile.py
+++ src/tile.py
@@ -5,13 +5,25 @@ from typing import List, Tuple
 
 import pygame
 
+from .config import config
+
 
 class Tile(pygame.sprite.Sprite):
     """docstring goes here"""
 
-    def __init__(self, pos: Tuple[int, int], groups: List[pygame.sprite.Group]) -> None:
+    def __init__(
+            self,
+            pos: Tuple[int, int],
+            groups: List[pygame.sprite.Group],
+            sprite_type,
+            surface: pygame.Surface=pygame.Surface((config.tilesize, config.tilesize))
+        ) -> None:
         super().__init__(groups)
 
-        self.image = pygame.image.load('lib/images/dummy/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=pos)
+        self.image = surface
+
+        if sprite_type == 'object':
+            self.rect = self.image.get_rect(topleft=(pos[0], pos[1] - config.tilesize))
+        else:
+            self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
