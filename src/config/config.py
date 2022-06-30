f10d87f897ef3ce0e060e2becbcec2e1e3294daf
GoodyeAAr <gauthier.cherchye@partenamut.be>
2022-06-30 10:53:22 +0200

    feat(config) add config for weapon animation

--- src/config/config.py
+++ src/config/config.py
@@ -16,5 +16,36 @@ class Configuration:
     fps = 60
 
     # Player config
+    player_img_path = 'lib/images/player/'
+    player_animation_speed = 0.15
     player_speed = 5
     player_attack_cooldown = 400
+
+    # Weapons config
+    weapon_data = {
+        'sword': {
+            'cooldown': 100,
+            'damage': 15,
+            'graphic': 'lib/images/weapons/sword/full.png'
+        },
+        'lance': {
+            'cooldown': 400,
+            'damage': 30,
+            'graphic': 'lib/images/weapons/lance/full.png'
+        },
+        'axe': {
+            'cooldown': 300,
+            'damage': 20,
+            'graphic': 'lib/images/weapons/axe/full.png'
+        },
+        'rapier': {
+            'cooldown': 50,
+            'damage': 8,
+            'graphic': 'lib/images/weapons/rapier/full.png'
+        },
+        'sai': {
+            'cooldown': 80,
+            'damage': 10,
+            'graphic': 'lib/images/weapons/sai/full.png'
+        }
+    }
