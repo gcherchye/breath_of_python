d9f16935678c09a4c53de3e7b955db4483eeac70
GoodyeAAr <gauthier.cherchye@partenamut.be>
2022-06-30 10:53:06 +0200

    feat(player): animate the player avatar

--- src/player.py
+++ src/player.py
@@ -5,7 +5,8 @@ from typing import List, Tuple
 
 import pygame
 
-from .config import config
+from src.config import config
+from src.utils.utils import import_image_from_folder
 
 
 class Player(pygame.sprite.Sprite):
@@ -24,43 +25,93 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -26)
 
+        # Loading animation
+        self.import_player_assets(config.player_img_path)
+        self.status = 'down'
+        self.frame_index = 0
+        self.animation_speed = config.player_animation_speed
+
         # Player initial attributes
         self.direction = pygame.math.Vector2()
         self.speed = config.player_speed
         self.attacking = False
         self.attack_cooldown = config.player_attack_cooldown
+        self.attack_time = None
 
         # Obstacles of the player for which we have to handle collision
         self.obstacles_sprite = obstacles
+
+    def import_player_assets(self, path:str):
+        """Docstring"""
+        self.animations = {
+            'up': [],
+            'down': [],
+            'left': [],
+            'right': [],
+            'up_idle': [],
+            'down_idle': [],
+            'left_idle': [],
+            'right_idle': [],
+            'up_attack': [],
+            'down_attack': [],
+            'left_attack': [],
+            'right_attack': [],
+        }
+
+        for animation in self.animations:
+            self.animations[animation] = import_image_from_folder(path + animation)
 
     def _input(self):
         """Identify the keys pressed by the user and modify the behaviour of the player avatar
         accordingly
         """
-        keys = pygame.key.get_pressed()
+        if not self.attacking:
+            keys = pygame.key.get_pressed()
 
-        # Movements input
-        if keys[pygame.K_z]:
-            self.direction.y = -1
-        elif keys[pygame.K_s]:
-            self.direction.y = 1
-        else:
+            # Movements input
+            if keys[pygame.K_z]:
+                self.direction.y = -1
+                self.status = 'up'
+            elif keys[pygame.K_s]:
+                self.direction.y = 1
+                self.status = 'down'
+            else:
+                self.direction.y = 0
+
+            if keys[pygame.K_d]:
+                self.direction.x = 1
+                self.status = 'right'
+            elif keys[pygame.K_q]:
+                self.direction.x = -1
+                self.status = 'left'
+            else:
+                self.direction.x = 0
+
+            # Attack input
+            if keys[pygame.K_SPACE] or keys[pygame.K_LCTRL]:
+                self.attacking = True
+                self.attack_time = pygame.time.get_ticks()
+
+    def _get_status(self):
+        """docstring"""
+        # Idle
+        if self.direction.x == 0 and self.direction.y == 0:
+            if not any(old in self.status for old in ['_idle', '_attack']):
+                self.status += '_idle'
+
+        if self.attacking:
+            self.direction.x = 0
             self.direction.y = 0
 
-        if keys[pygame.K_d]:
-            self.direction.x = 1
-        elif keys[pygame.K_q]:
-            self.direction.x = -1
+            if not '_attack' in self.status:
+                if '_idle' in self.status:
+                    self.status = self.status.replace('_idle', '_attack')
+                else:
+                    self.status += '_attack'
         else:
-            self.direction.x = 0
+            if '_attack' in self.status:
+                self.status = self.status.replace('_attack', '')
 
-        # Attack input
-        if keys[pygame.K_SPACE]:
-            print('attack')
-
-        # Magic input
-        if keys[pygame.K_LCTRL]:
-            print('magic')
 
     def _move(self, speed):
         """docstring"""
@@ -92,8 +143,32 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y <= 0:
                         self.hitbox.top = sprite.hitbox.bottom
 
+    def _cooldowns(self):
+        """docstring"""
+        current_time = pygame.time.get_ticks()
+        if self.attacking:
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
+
+    def _animate(self):
+        """docstring"""
+        animation = self.animations[self.status]
+
+        self.frame_index += self.animation_speed
+
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center=self.hitbox.center)
+
+
+
 
     def update(self):
         """docstring goes here"""
         self._input()
+        self._cooldowns()
+        self._get_status()
+        self._animate()
         self._move(self.speed)
