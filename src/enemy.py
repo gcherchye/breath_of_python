a315280b9e2d814753017e9b03060dece547c8b0
GoodyeAAr <gauthier.cherchye@partenamut.be>
2023-12-11 14:08:41 +0100

    feat(enemy): add graphics loading

--- src/enemy.py
+++ src/enemy.py
@@ -1,22 +1,37 @@
 """docstring"""
 from __future__ import absolute_import
-from typing import List
+
+from typing import List, Tuple
 
 import pygame
 
-from src.config import config
-from src.entity import Entity
+from .config import config
+from .entity import Entity
+from .utils.utils import import_image_from_folder
 
 
 class Enemy(Entity):
     """docstring"""
 
-    def __init__(self, monster_name: str, pos, groups: List[pygame.sprite.Group]) -> None:
+    def __init__(self, monster_name: str, pos: Tuple, groups: List[pygame.sprite.Group]) -> None:
         super().__init__(groups)
 
         # General setup
         self.sprite_type = 'enemy'
 
         # Graphics setup
-        self.image = pygame.Surface((64, 64))
+        self._import_graphics(monster_name)
+        self.status = 'idle'
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(topleft = pos)
+
+    def _import_graphics(self, name: str):
+        self.animations = {
+            'idle': [],
+            'move': [],
+            'attack': []
+        }
+        main_path = f'lib/images/monsters/{name}/'
+
+        for animation in self.animations:
+            self.animations[animation] = import_image_from_folder(main_path + animation)
