b714597c7ebb76d6f8f112b6e73968a2276a3c3f
GoodyeAAr <gauthier.cherchye@partenamut.be>
2023-12-11 08:17:13 +0100

    ref(config): improve layout

--- src/config/config.py
+++ src/config/config.py
@@ -41,31 +41,11 @@ class Configuration:
 
     # Weapons config
     weapon_data = {
-        'sword': {
-            'cooldown': 100,
-            'damage': 15,
-            'graphic': 'lib/images/weapons/sword/full.png'
-        },
-        'lance': {
-            'cooldown': 400,
-            'damage': 30,
-            'graphic': 'lib/images/weapons/lance/full.png'
-        },
-        'axe': {
-            'cooldown': 300,
-            'damage': 20,
-            'graphic': 'lib/images/weapons/axe/full.png'
-        },
-        'rapier': {
-            'cooldown': 50,
-            'damage': 8,
-            'graphic': 'lib/images/weapons/rapier/full.png'
-        },
-        'sai': {
-            'cooldown': 80,
-            'damage': 10,
-            'graphic': 'lib/images/weapons/sai/full.png'
-        }
+        'sword': {'cooldown': 100, 'damage': 15, 'graphic': 'lib/images/weapons/sword/full.png'},
+        'lance': {'cooldown': 400, 'damage': 30, 'graphic': 'lib/images/weapons/lance/full.png'},
+        'axe': {'cooldown': 300, 'damage': 20, 'graphic': 'lib/images/weapons/axe/full.png'},
+        'rapier': {'cooldown': 50, 'damage': 8, 'graphic': 'lib/images/weapons/rapier/full.png'},
+        'sai': {'cooldown': 80, 'damage': 10, 'graphic': 'lib/images/weapons/sai/full.png'}
     }
     weapon_horizon_offset = pygame.math.Vector2(0, 16)
     weapon_vertical_offset = pygame.math.Vector2(-10, 0)
