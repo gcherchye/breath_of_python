c3276af4487fba87a1abc0c66f07b06058af8390
GoodyeAAr <gauthier.cherchye@partenamut.be>
2023-12-11 13:27:15 +0100

    ref(player): move common method to entity & change inheritance

--- src/player.py
+++ src/player.py
@@ -5,11 +5,12 @@ from typing import Callable, List, Tuple
 
 import pygame
 
+from src.entity import Entity
 from src.config import config
 from src.utils.utils import import_image_from_folder
 
 
-class Player(pygame.sprite.Sprite):
+class Player(Entity):
     """A class representing the player in the game."""
 
     def __init__(
@@ -40,11 +41,8 @@ class Player(pygame.sprite.Sprite):
         # Loading animation
         self.import_player_assets(config.player_img_path)
         self.status = 'down'
-        self.frame_index = 0
-        self.animation_speed = config.player_animation_speed
 
         # Player initial attributes
-        self.direction = pygame.math.Vector2()
         self.attacking = False
         self.attack_cooldown = config.player_attack_cooldown
         self.attack_time = None
@@ -190,54 +188,6 @@ class Player(pygame.sprite.Sprite):
         else:
             if '_attack' in self.status:
                 self.status = self.status.replace('_attack', '')
-
-
-    def _move(self, speed: int) -> None:
-        """Moves the player's hitbox based on the specified speed and direction
-
-        Calculates the movement based on the player's current direction vector and applies the
-        movement to the hitbox. It checks for collisions in both horizontal and vertical directions
-        and adjusts the hitbox position accordingly.
-
-        Args:
-            speed (int): The speed at which the player moves.
-        """
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
-
-        self.hitbox.x += self.direction.x * speed
-        self._collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
-        self._collision('vertical')
-
-        self.rect.center = self.hitbox.center
-
-    def _collision(self, direction: str) -> None:
-        """Handles collision detection and response for the player's hitbox
-
-        Checks for collisions in the specified direction (horizontal or vertical) with obstacles
-        represented by sprites in the obstacles_sprite group. Adjusts the player's hitbox position
-        based on the detected collisions to prevent overlapping with obstacles.
-
-        Args:
-            direction (str): The direction in which collision detection is performed ('horizontal'
-            or 'vertical').
-        """
-        if direction == 'horizontal':
-            for sprite in self.obstacles_sprite:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x >= 0:
-                        self.hitbox.right = sprite.hitbox.left
-                    if self.direction.x <= 0:
-                        self.hitbox.left = sprite.hitbox.right
-
-        if direction == 'vertical':
-            for sprite in self.obstacles_sprite:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y >= 0:
-                        self.hitbox.bottom = sprite.hitbox.top
-                    if self.direction.y <= 0:
-                        self.hitbox.top = sprite.hitbox.bottom
 
     def _cooldowns(self) -> None:
         """Manages cooldowns for attacks and weapon switching.
