ae64e6a1f136629ae26a03b7c933096596b4fb40
GoodyeAAr <gauthier.cherchye@partenamut.be>
2023-12-11 13:52:08 +0100

    feat(enemy): add basic config to enemy

--- src/enemy.py
+++ src/enemy.py
@@ -4,11 +4,19 @@ from typing import List
 
 import pygame
 
+from src.config import config
 from src.entity import Entity
 
 
 class Enemy(Entity):
     """docstring"""
 
-    def __init__(self, groups: List[pygame.sprite.Group]) -> None:
+    def __init__(self, monster_name: str, pos, groups: List[pygame.sprite.Group]) -> None:
         super().__init__(groups)
+
+        # General setup
+        self.sprite_type = 'enemy'
+
+        # Graphics setup
+        self.image = pygame.Surface((64, 64))
+        self.rect = self.image.get_rect(topleft = pos)
