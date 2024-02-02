9ecaade6f2b4d93856e7d8b347128f4008a379f0
GoodyeAAr <gauthier.cherchye@partenamut.be>
2024-02-02 15:35:39 +0100

    fix(tile): add missing sprite type args

--- src/tile.py
+++ src/tile.py
@@ -28,6 +28,8 @@ class Tile(pygame.sprite.Sprite):
         """
         super().__init__(groups)
 
+        self.sprite_type = sprite_type
+
         self.image = surface
 
         if sprite_type == 'object':
